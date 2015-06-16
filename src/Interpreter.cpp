79a56003692792466b999e629507d5eb446e55fa
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2015-06-16 01:19:40 -0300

    Remove unecessary variable

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -7,12 +7,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
-  string filename = argv[1];
-  Tokenizer program(filename);
+  Tokenizer program(argv[1]);
   VarTable variables;
-
   Token token;
+
   while(program.Remaining()){
     token = program.GetToken();
     cout << "token: '" << token.GetValue() << "' type: " << token.GetType() << endl;
