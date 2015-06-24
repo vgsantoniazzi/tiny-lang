9c4f816e795cdbd56922288434dc86c7b7ae2ce6
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2015-06-23 23:48:42 -0300

    Support full math expressions

--- src/Evaluate.cpp
+++ src/Evaluate.cpp
@@ -1,6 +1,11 @@
-﻿#include "Token.h"
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "Token.h"
 #include "Tokenizer.h"
 #include "Evaluate.h"
+
+using namespace std;
 
 int Evaluate::Calculate(Tokenizer & program)
 {
@@ -11,24 +16,59 @@ int Evaluate::Calculate(Tokenizer & program)
 int Evaluate::Expression(Tokenizer & program)
 {
   int val;
-  if(program.Look().GetType() == ADD)
-  {
+  if(program.Look().GetType() == ADD || program.Look().GetType() == SUB)
     val = 0;
-  }
-    else
+  else
+    val = Term(program);
+  while(program.Look().GetType() == ADD || program.Look().GetType() == SUB)
   {
-    val = atoi(program.GetToken().GetValue().c_str());
+    if(program.Look().GetType() == ADD)
+    {
+      program.Match(ADD);
+      val += Term(program);
+    }
+    else if(program.Look().GetType() == SUB)
+    {
+      program.Match(SUB);
+      val -= Term(program);
+    }
   }
   return val;
 }
 
 int Evaluate::Term(Tokenizer & program)
 {
-  return 1;
+  int val = Factor(program);
+
+  while(program.Look().GetType() == MULT || program.Look().GetType() == DIVIDE)
+  {
+    if(program.Look().GetType() == MULT)
+    {
+      program.Match(MULT);
+      val *= Term(program);
+    }
+    else if(program.Look().GetType() == DIVIDE)
+    {
+      program.Match(DIVIDE);
+      val /= Term(program);
+    }
+  }
+  return val;
 }
 
 int Evaluate::Factor(Tokenizer & program)
 {
-  return 1;
+  int val;
+  if(program.Look().GetType() == OPEN_PARENTHESYS)
+  {
+    program.Match(OPEN_PARENTHESYS);
+    val = Expression(program);
+    program.Match(CLOSE_PARENTHESYS);
+  }
+  else
+  {
+   val = atoi(program.GetToken().GetValue().c_str());
+  }
+  return val;
 }
 
